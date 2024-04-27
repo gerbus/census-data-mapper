37187d559e3aba1f7f25e3e3c20548b981088033
gerbus <chris.gerber@jane.app>
2024-04-27 07:59:08 -0700

    feat: add 3 more metrics (household incomes)

--- data/data-scripts/index.js
+++ data/data-scripts/index.js
@@ -2,10 +2,15 @@
 
 
 // Processing Census Data
-// 1. Run filter-csv-by-characteristic-ids on csv saved from census canada. Cuts the size of the data down significantly for subsequent processing, by selecting a subset of characteristics. Outputs a new (filtered) csv.
-// 2. Run transform-csv-to-json on csv from above. Massages the data into a more workable json format. Outputs a json file.
-// 3. Run calculate-derived-metrics-and-save on json file above. Calculates percentages and adds to the data and data model. Outputs new json file and a csv file.
+// 1. Run filter-csv-by-characteristic-ids on csv saved from census canada. Cuts the size of the data down significantly for subsequent processing, by selecting a subset of characteristics. Outputs a new (filtered) csv (canada-census-2021-bc-filtered.csv)
+// 2. Run transform-csv-to-json on csv from above. Massages the data into a more workable json format. Outputs a json file (canada-census-2021-bc-filtered.json)
+// 3. Run calculate-derived-metrics-and-save on json file above. Calculates percentages and adds to the data and data model. Outputs new json file and a csv file (canada-census-2021-bc-derived.*)
+// 4. After this, need to upload canada-census-2021-bc-derived.json to server.
+//    cd data
+//    scp canada-census-2021-bc-derived.json gerbus@gerbus.ca:/gerbus.ca/node/census-data-mapper/server/assets/canada-census-2021-bc-derived.json
 
 
 // Processing Boundary Data
 // 1. Run transform-gml-to-json on boundary file data from census canada. Pulls out the posList for each DGUID (ALT_GEO_CODE), converts that to an array of coordinates for each DGUID. Outputs json file.
+// 2. After this, need to upload to server
+//    scp canada-census-2021-boundary-data.json gerbus@gerbus.ca:/gerbus.ca/node/census-data-mapper/server/assets/canada-census-2021-boundary-data.json
